00b39680f7bf4303ac2f5ef3515131ce11f2c658
Saritha <sarithapillai@hq.microobjects.net>
2020-04-04 09:59:13 +0530

    Updated node inde to handle different games from different folders

--- MindLamp-Games/Games/images.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.svg'
-declare module '*.png'
-declare module '*.jpg'
-declare module 'react-router-dom'
-declare module 'react-dom'
-declare module 'react-addons-css-transition-group'
-declare module 'react-animations'
-declare module 'styled-components'
--- MindLamp-Games/index.js
+++ MindLamp-Games/index.js
@@ -1,31 +1,45 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
-var cors = require('cors');
-const bodyParser = require('body-parser')
+let port = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
-
-app.use(bodyParser.json())
-
-
-// Then use it before your routes are set up:
-app.use(cors());
-let auth = '';
-let username = '';
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'Games/build')));
-
-
-// Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-	res.sendFile(path.join(__dirname+'/Games/build/index.html'));
+const box = express();
+box.use(express.static(path.join(__dirname, 'Box/build')));
+box.get('/box', (req,res) =>{
+	res.sendFile(path.join(__dirname+'/Box/build/index.html'));
 });
+box.listen(port);
+console.log('/box is listening on port ' + port);
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+port = process.env.PORT || 5001;
 
-console.log('App is listening on port ' + port);
+const jewels = express();
+jewels.use(express.static(path.join(__dirname, 'Jewels/build')));
+jewels.get('/jewels', (req,res) =>{
+	res.sendFile(path.join(__dirname+'/Jewels/build/index.html'));
+});
+jewels.listen(port);
+console.log('/jewels is listening on port ' + port);
+
+port = process.env.PORT || 5002;
+
+const catsndogs = express();
+catsndogs.use(express.static(path.join(__dirname, 'CatsnDogs/build')));
+catsndogs.get('/catsndogs', (req,res) =>{
+	res.sendFile(path.join(__dirname+'/CatsnDogs/build/index.html'));
+});
+catsndogs.listen(port);
+
+console.log('/catsndogs is listening on port ' + port);
+
+port = process.env.PORT || 5003;
+
+const popupbubbles = express();
+popupbubbles.use(express.static(path.join(__dirname, 'PopTheBubbles/build')));
+popupbubbles.get('/popthebubbles', (req,res) =>{
+	res.sendFile(path.join(__dirname+'/PopTheBubbles/build/index.html'));
+});
+popupbubbles.listen(port);
+
+console.log('/popthebubbles is listening on port ' + port);
