d4ade74801b72280244aa326ec9c6f5aceeb468c
Saritha <sarithapillai@hq.microobjects.net>
2020-09-03 21:55:51 +0530

    game fixes

--- BalloonRisk/webpack.config.js
+++ BalloonRisk/webpack.config.js
@@ -1,25 +1,25 @@
 const path = require("path")
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
+//const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
 const glob = require("glob")
 
 module.exports = {
   entry: {
     "bundle.js": glob.sync("build/static/?(js|css)/main.*.?(js|css)").map(f => path.resolve(__dirname, f)),
-  },
-  output: {
-    filename: "build/static/js/bundle.min.js",
-  },
+  }, 
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       }, 
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      { 
         loader: "file-loader",
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,       
     },
     ],
   },
-  plugins: [new UglifyJsPlugin()],
+  output: {
+    filename: "build/static/js/bundle.min.js",
+  },
+  //plugins: [new UglifyJsPlugin()],
 }
