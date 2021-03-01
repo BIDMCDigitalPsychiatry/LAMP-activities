1abe8d1a5b9eceaf59cbcd64a1ebd9c04a299ca8
Saritha <sarithapillai@hq.microobjects.net>
2021-03-01 19:58:29 +0530

    DBT blank page issue fix by removing routes

--- DBTDiaryCard/src/index.js
+++ DBTDiaryCard/src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from './containers/home'
-import { BrowserRouter } from 'react-router-dom'
+import { AppContainer } from "react-hot-loader";
+import Patient from "./containers/patient/index"
 import './i18n'
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <AppContainer>
+    <Provider store={store}>
+      <Patient />
+    </Provider>
+  </AppContainer>,
   document.getElementById("root")
 );
--- DBTDiaryCard/src/redux/store.js
+++ DBTDiaryCard/src/redux/store.js
@@ -1,32 +1,19 @@
-import { applyMiddleware, compose, createStore } from 'redux';
-import { createBrowserHistory as createHistory } from 'history'
-import { routerMiddleware, routerReducer } from 'react-router-redux'
+import { compose, createStore } from 'redux';
 import reducers from './reducers'
 import { persistStore, persistCombineReducers } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
-
-const history = createHistory();
-const routeMiddleware = routerMiddleware(history)
-
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-    })
-    : compose;
 
 const persistConfig = {
   key: 'root',
   storage,
 }
 
-const persistedReducer = persistCombineReducers(persistConfig, {...reducers, router: routerReducer})
+const persistedReducer = persistCombineReducers(persistConfig, {...reducers})
 
 const store = createStore(
-  persistedReducer,
-  composeEnhancers(applyMiddleware(routeMiddleware))
+  persistedReducer 
 );
 
 let persistor = persistStore(store)
 
-export { store, history, persistor };
+export { store, persistor };
