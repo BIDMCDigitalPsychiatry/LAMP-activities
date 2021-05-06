a65006e38fcbb92ab3d6cb531f4264b84c8a8079
Aditya Vaidyam <avaidyam@bidmc.harvard.edu>
2021-05-06 09:42:56 -0400

    Rebuild all activities.
    - Updated buildscript with support for specifying directories and handling build errors (skip).
    - Updated all lockfiles.

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash 
 
+# Optionally specify which directories only to build.
+ALL_ARGS=${*:1} # Join all args as one big string.
+UNIT_LIST=${ALL_ARGS:-*/} # If no args provided, use */ glob instead.
+
 # All directories with a package.json are treated as build units.
-for dir in */; do
+for dir in $UNIT_LIST; do
 	if [ -f $dir/package.json ]; then
 		pushd $dir
 
@@ -10,11 +14,11 @@ for dir in */; do
 		echo "Building $name..."
 
 		# Install & Build
-		npm install 
-		npm run build
+		npm install || continue
+		npm run build || continue
 
 		# Compress for distribution and move to /dist/ directory.
-		../dist/compress_activity.sh
+		../dist/compress_activity.sh || continue
 		mv dist.html ../dist/in/$name.html
 		mv dist.html.b64 ../dist/out/$name.html.b64
 
