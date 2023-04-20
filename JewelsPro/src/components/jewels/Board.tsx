32996c8b35b54fe7e3165ef5286d20e6a3de7449
ITHQ1947\saritha.pillai <saritha.pillai@zcocorporation.com>
2023-04-20 19:20:03 +0530

    Jewels issue and box UI

--- JewelsPro/src/components/jewels/Board.tsx
+++ JewelsPro/src/components/jewels/Board.tsx
@@ -240,7 +240,7 @@ class Board extends React.Component<BoardProps, DiamondState> {
     let p = 0;
     const rows = 9
     const cols = 6
-    const height = (window.innerHeight - (window.innerHeight * 15) / 100) / 12
+    const height = (window.innerHeight - (window.innerHeight * 15) / 100) / 12.5
     // let diamondStyle = this.props.currentDiamond[0]
     // Outer loop to create parent
     for (let i = 0; i < rows; i++) {
