fd3cbb6af6784a563e4f29e65cfcb9104a22169e
ITHQ1947\saritha.pillai <saritha.pillai@zcocorporation.com>
2023-07-26 17:59:16 +0530

    Balloon risk changes

--- BalloonRisk/src/index.tsx
+++ BalloonRisk/src/index.tsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import { AppContainer } from "react-hot-loader";
 import Balloon from './components/BallonRisk/Balloon';
 
-ReactDOM.render(
-  <AppContainer>
-    <Balloon/>
-  </AppContainer>,
-  document.getElementById('root') as HTMLElement
-);
+const eventer = window.addEventListener
+const messageEvent = "message"
+eventer(
+  messageEvent, (e:any) => {  
+    ReactDOM.render(
+      <AppContainer>
+        <Balloon data={e.data}/>
+      </AppContainer>,
+      document.getElementById('root') as HTMLElement
+      ) 
+    },
+false
+)
