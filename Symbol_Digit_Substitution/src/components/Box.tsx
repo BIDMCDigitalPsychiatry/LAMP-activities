1481e8ad1ccd26560829ecb0a5d409454aa29321
ITHQ1947\saritha.pillai <saritha.pillai@zcocorporation.com>
2023-08-22 19:01:22 +0530

    Game changes as per requirement

--- Symbol_Digit_Substitution/src/components/Box.tsx
+++ Symbol_Digit_Substitution/src/components/Box.tsx
@@ -4,6 +4,7 @@ import {
     makeStyles,
     Grid,
     ListItem,
+    Button,
 } from "@material-ui/core"
 
 const useStyles = makeStyles((theme) => ({
@@ -16,32 +17,44 @@ const useStyles = makeStyles((theme) => ({
         borderRadius: 10,
         [theme.breakpoints.down('md')]: {
             margin: "5px",
-          },
+        },
         "& li": {
             fontSize: 22,
             justifyContent: "center"
         },
-   
+
     },
 
     divider: {
         borderTop: "2px solid white",
         margin: "3px 0",
     },
+    btnstyle: {
+        padding: "11px 15px",
+        color: "white",
+        '&:hover': {
+            color: 'black',
+        },
+    }
 }))
 
 export default function Box({ ...props }) {
+    const handleButtonClick = (data: any) => {
+        props.handleClick(data)
+    }
     const classes = useStyles()
-       return (
-                        <Grid container className={props.className} >
-                            {props?.data.map((value: any, index: number) => (
-                                <Grid className={classes.box} item xs={2} sm={2} md={1} key={index}>
-                                    {props?.type?  <ListItem>{value.number}</ListItem>  : <ListItem>{value}</ListItem> }
-                                    {props?.type ? null : <div className={classes.divider} />  }
-                                    {props?.type?  <ListItem>{"["}{value.keyvalue}{"]"}</ListItem>  :   <ListItem>{index + 1}</ListItem> }
-                                </Grid>
-                            ))}
-                        </Grid>
-                                 
+    return (
+
+
+        <Grid container className={props.className}>
+            {!!props?.data ? props?.data.map((value: any, index: number) => (
+                <Grid className={classes.box} item xs={2} sm={2} md={1} key={index}>
+                    {props?.type ? <ListItem><Button disabled={props.flag !== 2} onClick={(event) => handleButtonClick(index + 1)} className={classes.btnstyle}>{index + 1}</Button></ListItem> : <ListItem>{value}</ListItem>}
+                    {props?.type ? null : <div className={classes.divider} />}
+                    {props?.type ? null : <ListItem>{index + 1}</ListItem>}
+                </Grid>
+            )) : <Grid className={classes.box} item xs={2} sm={2} md={1} > <ListItem>{props?.currentSymbol}</ListItem></Grid>}
+        </Grid>
+
     )
 }
