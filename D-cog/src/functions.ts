29b6ac67faf6c0a1bb38c9542b8e1e025841065c
ITHQ1947\saritha.pillai <saritha.pillai@zcocorporation.com>
2024-09-03 20:28:51 +0530

    D-cog game changes

---
+++ D-cog/src/functions.ts
@@ -0,0 +1,15 @@
+ // Get random numbers
+export function getRandomNumbers (dcount:number, min:number, max:number) {
+    const randomArray:Array<number> =  []  ;
+    for(let i = min; i <= dcount; i++) {      
+      randomArray[i-1] = randomNumber(max, 1, randomArray)       
+    }       
+    return randomArray;
+  }
+// recursive random number generation without any duplicate
+function randomNumber  (max:number, min:number, randomArray:Array<number>): number {
+const num = Math.floor(Math.random() * (max - min +1)) + min;
+return randomArray.indexOf(num) >= 0 ? randomNumber(max, min, randomArray) : num;
+}
+
+ 
