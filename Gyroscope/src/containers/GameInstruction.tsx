b2ddac60f56bdc7a6e2ded585b234088843f47da
ITHQ1947\saritha.pillai <saritha.pillai@zcocorporation.com>
2024-10-07 18:24:17 +0530

    Gyroscope - add instruction popup

--- Gyroscope/src/containers/GameInstruction.tsx
+++ Gyroscope/src/containers/GameInstruction.tsx
@@ -81,19 +81,21 @@ export default function Instructions({ ...props }) {
   const classes = useStyles();
   const [noBack] = useState(false);
   const { t } = useTranslation();
-  const[showModal,setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const [view, setView] = useState("");
   const handleNextClick = () => {
     setView("start game");
   };
 
-  useEffect(()=>{
-    if(isMobile)setShowModal(true)
-      else{setView("instruction")}
-  },[])
+  useEffect(() => {
+    if (isMobile) setShowModal(true);
+    else {
+      setView("instruction");
+    }
+  }, []);
 
   return (
-    <div className={classes.root}>      
+    <div className={classes.root}>
       {view === "instruction" && (
         <Box p={0} className={classes.container}>
           <Grid
@@ -105,9 +107,9 @@ export default function Instructions({ ...props }) {
             <Grid item lg={7} sm={10} xs={12}>
               <Box p={3}>
                 <Typography variant="body1" align="center">
-                  Move the white dot to the center. The white dot will be
-                  visible during your reach. Quickly move your white dot to the
-                  target. Press SPACE BAR when you are ready to proceed.
+                  {isMobile
+                    ? "Keep the device horizontal before tapping the center circle"
+                    : "Move the white dot to the center. The white dot will be visible during your reach. Quickly move your white dot to the target. Press SPACE BAR when you are ready to proceed."}
                 </Typography>
                 <Box textAlign="center" pt={3} mt={2}>
                   <Fab className={classes.btnBlue} onClick={handleNextClick}>
@@ -118,15 +120,22 @@ export default function Instructions({ ...props }) {
             </Grid>
           </Grid>
         </Box>
-      )}      
+      )}
       <InstructionModal
-      show={showModal}
-      onHide={()=>{setShowModal(false); setView("instruction")}}
-      message={"Please use Portrait  Orientation"}
-      />     
+        show={showModal}
+        onHide={() => {
+          setShowModal(false);
+          setView("instruction");
+        }}
+        message={"Please use Portrait  Orientation"}
+      />
 
       {view === "start game" && (
-        <GameComponent adventure="Medium" setView={setView} clickBack={props.clickBack}/>
+        <GameComponent
+          adventure="Medium"
+          setView={setView}
+          clickBack={props.clickBack}
+        />
       )}
       {view === "end game" && <GameEnd />}
     </div>
