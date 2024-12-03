55a85e829018580c6f9163fd503525aecb03aa5d
ITHQ1947\saritha.pillai <saritha.pillai@zcocorporation.com>
2024-12-03 18:34:41 +0530

    Speech recording updates

---
+++ SpeechRecording/src/components/speech_recording/images/stop.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Stop = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="173" height="173" viewBox="0 0 173 173">
+      <path id="stop" d="M22,13a9.01,9.01,0,0,0-9,9V151a9.01,9.01,0,0,0,9,9H151a9.01,9.01,0,0,0,9-9V22a9.01,9.01,0,0,0-9-9H22M22,0H151a22,22,0,0,1,22,22V151a22,22,0,0,1-22,22H22A22,22,0,0,1,0,151V22A22,22,0,0,1,22,0Z" fill="#fff"/>
+    </svg>
+  )
+}
+
+export default Stop
--- SpeechRecording/src/index.tsx
+++ SpeechRecording/src/index.tsx
@@ -32,3 +32,5 @@ eventer(
     false
   );
 
+
+
