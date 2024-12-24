2fd7c4e954c7de8ad34f7326c9f5a1b1a9c64e98
ITHQ1947\saritha.pillai <saritha.pillai@zcocorporation.com>
2024-12-24 12:12:30 +0530

    Embed video issue fix

--- Tips/src/index.tsx
+++ Tips/src/index.tsx
@@ -1,26 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import LearnTips from "./components/LearnTips";
-import reportWebVitals from "./reportWebVitals";
 
-const eventMethod: any = window.addEventListener !== undefined ? "addEventListener" : "attachEvent";
-const eventer: any = window[eventMethod];
-const messageEvent = eventMethod === "attachEvent" ? "onmessage" : "message";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import * as React from 'react';
+import { AppContainer } from "react-hot-loader";
+import LearnTips from "./components/LearnTips";
+import './index.css';
+import { createRoot } from "react-dom/client";
+const eventMethod = "addEventListener"
+const eventer = window[eventMethod]
+const messageEvent = "message"
+
 eventer(
-  messageEvent,
-  (e: any) => {
-    ReactDOM.render(
-      <React.StrictMode>
+  messageEvent, (e: any) => {
+    const rootElement = document.getElementById("root") as HTMLElement;
+
+    if (!!rootElement) {
+      const root = createRoot(rootElement);
+      root.render(<AppContainer>
         <LearnTips data={e.data} />
-      </React.StrictMode>,
-      document.getElementById("root")
-    );
+      </AppContainer>);
+    }
   },
   false
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
